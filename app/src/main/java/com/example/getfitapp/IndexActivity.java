4a15d94223689c94f00c5de43f25035ab67aa937
Cyrell Funtanilla <mcfuntanilla@tip.edu.ph>
2023-05-04 17:10:46 +0800

    Fifth Commit Index & Programs

--- app/src/main/java/com/example/getfitapp/IndexActivity.java
+++ app/src/main/java/com/example/getfitapp/IndexActivity.java
@@ -2,14 +2,35 @@ package com.example.getfitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IndexActivity extends AppCompatActivity {
-
+        Button index, program;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_index);
+
+        index = (Button) findViewById(R.id.index1);
+        program = (Button) findViewById(R.id.prog1);
+
+        index.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IndexActivity.this, IndexActivity.class));
+            }
+        });
+
+        program.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IndexActivity.this, ProgramsActivity.class));
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/getfitapp/ProgramsActivity.java
+++ app/src/main/java/com/example/getfitapp/ProgramsActivity.java
@@ -2,13 +2,35 @@ package com.example.getfitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProgramsActivity extends AppCompatActivity {
-
+        Button index,program;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_programs);
+
+        index = (Button) findViewById(R.id.index2);
+        program = (Button) findViewById(R.id.prog2);
+
+        index.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProgramsActivity.this,IndexActivity.class));
+            }
+        });
+
+        program.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProgramsActivity.this, ProgramsActivity.class));
+            }
+        });
+
     }
 }
